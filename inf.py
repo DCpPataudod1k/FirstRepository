5e075c68c47e09fdbe03c3fedfc959b0b522c96b
DCpPataudod1k <115190568+DCpPataudod1k@users.noreply.github.com>
2022-10-07 17:50:25 +0300

    Update inf.py
    Добавить содержание отчета

--- inf.py
+++ inf.py
@@ -5,3 +5,7 @@ def mirror_sum_2(a:list, b:list) -> list:
     return [u + v for u,v in zip(a,reversed(b))];
 
 #ДОБАВЛЕНИЕ ИНФОРМАЦИ
+Содержание отчета
+1. Цель работы.
+2. Назначения GitHub.
+3. Созданный аккаунт на GitHub с календарем активностей
